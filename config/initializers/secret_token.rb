ec0a24767bd354f7fda20879d7cad244c932d07f
Barty Kim <barty82@naver.com>
2013-01-31 13:56:30 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArirangHouseROR::Application.config.secret_token = 'ac206a41d0931894401361b6137524ed096fe267f24dd1d58d7d8bdbae7bf3b9185eebe8b76a9169c49ca838607dd2a3304f6ef28d5b2e209ac2eb28746cf1ff'
