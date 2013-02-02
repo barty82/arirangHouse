33711dc54f1e074f51cf34c00b2e9270d28608d7
Barty Kim <barty82@naver.com>
2013-02-01 17:17:22 -0800

    Added new pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ArirangHouseROR::Application.routes.draw do
+  get "about" => 'pages#about'
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
